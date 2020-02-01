72a6d108896966bb0c5fe30375b88f6bba410e19
Bryden Frizzell <bryden.frizzell@gmail.com>
2020-02-02 00:29:41 +1300

    Scavengers can now pick stuff up

--- Assets/Scripts/ScavengerRobot.cs
+++ Assets/Scripts/ScavengerRobot.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class ScavengerRobot : MonoBehaviour
 {
+	public GameObject pickupPoint;
+
 	private const float Speed = 10.0f;
 
 	private Controller controller;
 
 	private Part part;
 
+	private List<Part> pickableParts;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		pickableParts = new List<Part>();
+
+	}
 
     // Update is called once per frame
     void Update()
@@ -32,12 +37,62 @@ public class ScavengerRobot : MonoBehaviour
 
 		if (controller.IsDoingPickUp())
 		{
+			Part bestPart = null;
+			float bestDistance = float.PositiveInfinity;
 
+			foreach(Part part in pickableParts)
+			{
+				Vector3 point = part.GetComponentInChildren<Collider>().ClosestPoint(transform.position);
+				float distance = (transform.position - point).magnitude;
+
+				if(distance < bestDistance)
+				{
+					bestPart = part;
+					bestDistance = distance;
+				}
+			}
+
+			if (part != null)
+			{
+				part.transform.parent.gameObject.GetComponent<Rigidbody>().detectCollisions = true;
+				part = null;
+			}
+
+			if (bestPart != null)
+			{
+				part = bestPart;
+				part.transform.parent.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+			}
 		}
+
+		if (part != null)
+		{
+			part.transform.parent.gameObject.transform.position = pickupPoint.transform.position;
+			part.transform.parent.gameObject.transform.rotation = pickupPoint.transform.rotation;
+		}
+			
 	}
 
 	public void SetController(Controller controller)
 	{
 		this.controller = controller;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Part part = other.GetComponentInParent<Part>();
+
+		Debug.Log(part);
+
+		if (part != null)
+			pickableParts.Add(part);
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log(other);
+		Part part = other.GetComponentInParent<Part>();
+
+		if (part != null)
+			pickableParts.Remove(part);
 	}
 }
